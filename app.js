2b628e5e678089452068f0c23800f1f99a517005
mean <meanstack4@gmail.com>
2025-06-25 14:29:00 +0530

    1st deployment

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import UserRouter from "./routes/user.route.js";
+import bodyParser from "body-parser";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+dotenv.config();
+const app = express();
+
+
+mongoose.connect(process.env.DB_URL)
+.then(result=>{
+   app.use(cors());
+   app.use(cookieParser()); 
+   app.use(bodyParser.json());
+   app.use(bodyParser.urlencoded({extended: true})); 
+   app.use("/user",UserRouter); 
+   app.listen(process.env.PORT||3000,()=>{
+    console.log("Server Started....");
+   });
+}).catch(err=>{
+    console.log(err);
+    console.log("Database connection failed...");
+})
+
